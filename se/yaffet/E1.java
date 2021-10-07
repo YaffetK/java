7d1ca4b73b449b1d579313fb13313df2277dcbf6
yaffet97 <yaffet97@gmail.com>
2021-10-07 13:13:23 +0200

    a

--- se/yaffet/E1.java
+++ se/yaffet/E1.java
@@ -2,7 +2,22 @@ package se.yaffet;
 
 public class E1 {
     public void nummerlista(){
-     int[] lista={12,55,32,9,34};
+     int[]lista={12,55,32,9,34};
+     int largest = lista[0];
+        int smallest = lista[0];
+
+        for(int tal : lista){
+            if(tal < smallest)
+                smallest  = tal;
+            if(tal > largest)
+                largest = tal;
+        }
+        System.out.printf("Största:%d Minsta:%d", largest,
+                 smallest);
+
+    
+     
+
      
 
 
---
+++ se/yaffet/E2.java
@@ -0,0 +1,21 @@
+package se.yaffet;
+
+import java.util.ArrayList;
+
+public class E2 {
+    public void lista(){
+
+         ArrayList<Integer> tal1 = new ArrayList<>();
+         tal1.add(12);
+         tal1.add(55);
+         tal1.add(32);
+         tal1.add(9);
+         tal1.add(34);
+
+         ArrayList<Integer> tal2 = new ArrayList<>(Arrays.asList( 12,55,32,9,34));
+
+    
+         
+    }
+    
+}
