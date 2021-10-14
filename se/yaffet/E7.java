767161e34470bccc380285e9695eb9f6fe5ceb85
yaffet97 <yaffet97@gmail.com>
2021-10-14 23:12:37 +0200

    ah

---
+++ se/yaffet/E7.java
@@ -0,0 +1,31 @@
+package se.yaffet;
+
+
+public class E7 {
+    public void run(){
+        
+        System.out.print("Skriv in en text:");
+        String text = System.console().readLine();
+
+        char mostCommon=' ';
+        int mostCommonCount = 0;
+
+        for(int i = 0; i < text.length(); i++){
+            char currentToCheck = text.charAt(i);
+
+            int cnt = 0;
+            for(int j = 0; j < text.length(); j++){
+                if(text.charAt(j) == currentToCheck)                    
+                    cnt++;
+            }
+
+            if(cnt > mostCommonCount)
+            {
+                mostCommonCount = cnt;
+                mostCommon = currentToCheck;
+            }
+
+        }
+        System.out.printf("%c   %d gånger\n", mostCommon, mostCommonCount);
+    }
+}
