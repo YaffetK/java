3093079b397ec87a57441a9d7f31f362ad13c2ed
yaffet97 <yaffet97@gmail.com>
2021-10-16 19:00:31 +0200

    a

---
+++ se/yaffet/D7.java
@@ -0,0 +1,39 @@
+package se.yaffet;
+
+public class D7 {
+    public void run(){
+        System.out.print("Mata in ett namn:");
+        String input = System.console().readLine();
+
+        String result = "";
+        boolean first = true;
+        boolean lastWasSpace = false;
+
+        for(int i = 0; i < input.length(); i++){
+            char ch = input.charAt(i);
+            if(first || lastWasSpace){
+                result = result + Character.toUpperCase(ch);
+            }
+            else
+                result = result + Character.toLowerCase(ch);
+
+            lastWasSpace = ch == ' ';
+            first = false;
+
+
+        }
+
+
+
+        System.out.printf("%s ->  %s\n",input, result);
+
+    }
+
+}
+
+
+        
+
+    }
+    
+}
